5ce39f000acf8448a2c5793d84c748fb7b10804e
Pierrick Bruneau <pierrick.bruneau@list.lu>
2023-10-15 12:20:01 +0200

    fixed all mixture_same_family issues

--- src/gluonts/torch/distributions/mixture_same_family.py
+++ src/gluonts/torch/distributions/mixture_same_family.py
@@ -55,7 +55,7 @@ class MixtureArgs(nn.Module):
                 component_args.append(c_proj[i])
 
         # mixture probs: (batch_size, nsteps, ncomp)
-        # component_args: list with ncomp*nparams (batch_size, nsteps) (respective order)
+        # component_args: list with ncomp*nparams (batch_size, nsteps) (ncomp major)
         
         #pdb.set_trace()
         return tuple([mixture_probs] + component_args)
@@ -85,57 +85,49 @@ class MixtureSameFamilyOutput(DistributionOutput):
         # and flattened component structure
         mixture_probs = distr_args[0]
         component_args = distr_args[1:]
+        batch_size = distr_args[0].shape[0]
+        nsteps = distr_args[0].shape[1]
         
         #pdb.set_trace()
         
-        # mixture_distribution expects (batch_size, ncomp)
-        # mixture_probs is (nhidden, 1, ncomp)
+        # mixture_distribution expects (batch_size, nsteps, ncomp)
+        # mixture_probs is (batch_size, nsteps, ncomp)
         
-        # component_distribution argument of MixtureSameFamily expects (batch_size, ncomp, nparam)        
-        # component_args is list with ncomp*nparams (nhidden, 1)
+        # component_distribution argument of MixtureSameFamily expects list (batch_size, nsteps, ncomp)
+        # component_args is list with ncomp*nparams (batch_size, nsteps)
         
         # in MXNet, mixture_probs is (batch_size, nsteps, ncomp) 
         # component_args has cells (batch_size, nstep) at this stage
         # loc is None
         # scale is (batch_size, 1)
         
-        
-        # with NormalOutput, list nparams (nhidden, 1), passed to base distribution constructor (which expands it),
+        # with NormalOutput, list nparams (batch_size, nsteps), passed to base distribution constructor (which expands it),
         # so output is consistent
         # loc is None
-        # shape is (nhidden, 1)
-        # !!! in inference mode, the shape is not the same at all: becomes (nbatch, nsteps)
-        # and mixture params are (nbatch, nsteps, ncomp), which is incompliant with pytorch mixture_same_family
-        # need to flatten and unflatten
-        # and shape is (nbatch, 1)
-        # so the 1 in initialization may be expanded to arbitrary size (trailing 1 has to be accounted for)
-        # in the end, nhidden must be taken homogeneously to batch size
-        # consistent with observations with mixture: list ordering system unchanged
-        # just account correctly for additional dim
+        # shape is (batch_size, 1)
         
         # for consistency with single output and class signature, argments to MixtureSameFamily should be:
-        # mixture_distribution: (nhidden, ncomp)
-        # component_distribution: list nparam (nhidden, ncomp)
-        # (if fails, leaving the trailing 1)
-        
-        nhidden = mixture_probs.shape[0]
+        # mixture_distribution: (batch_size, nsteps, ncomp)
+        # component_distribution: list nparam (batch_size, nsteps, ncomp)
         
         comp_args_concat = []
         for j in range(self.per_comp_len):
             #if i==0 and j==0:
             #    pdb.set_trace()
-            tensor = torch.empty((nhidden, self.num_components))
+            tensor = torch.empty((batch_size, nsteps, self.num_components))
             
             for i in range(self.num_components):
                 index = i*self.per_comp_len + j
-                tensor[:, i] = component_args[index].squeeze(dim=1)
+                tensor[:, :, i] = component_args[index]
             comp_args_concat.append(tensor)
         
         #pdb.set_trace()
         
         return MixtureSameFamily(
-            mixture_distribution = Categorical(mixture_probs.squeeze(dim=1)),
-            component_distribution = self.distr_outputs[0].distribution(comp_args_concat, loc=loc, scale=scale)
+            mixture_distribution = Categorical(mixture_probs),
+            # unsqueeze so that scale matches the mixture specification
+            component_distribution = self.distr_outputs[0].distribution(comp_args_concat, loc=loc, \
+                                                                        scale=scale.unsqueeze(-1))
         )
 
     @property
